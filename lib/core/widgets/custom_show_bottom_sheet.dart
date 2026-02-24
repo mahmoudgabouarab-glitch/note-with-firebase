04b8eecac372d465febda63be3b767ecf68e8620
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-02-25 01:21:04 +0200

    edit name project

--- lib/core/widgets/custom_show_bottom_sheet.dart
+++ lib/core/widgets/custom_show_bottom_sheet.dart
@@ -14,6 +14,7 @@ class CustomShowBottomSheet {
     void Function()? ontap,
     bool isEdit = false,
   }) {
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
     return showModalBottomSheet<T?>(
       useSafeArea: true,
       isScrollControlled: true,
@@ -30,54 +31,64 @@ class CustomShowBottomSheet {
         child: SizedBox(
           width: double.infinity,
           height: MediaQuery.of(context).size.height * 0.40,
-          child: Column(
-            children: [
-              spaceH(8),
-              Center(
-                child: Container(
-                  width: 64.w,
-                  height: 4.h,
-                  color: AppColor.primary,
-                ),
-              ),
-              spaceH(20),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("title"),
-                      spaceH(8),
-                      CustomTextFiled(
-                        hint: "title",
-                        controller: titleController,
-                      ),
-                      spaceH(20),
-                      Text("subtitle"),
-                      spaceH(8),
-                      CustomTextFiled(
-                        hint: "subtitle",
-                        controller: subTitleController,
-                      ),
-                      spaceH(30),
-                      Center(
-                        child: SizedBox(
-                          width: 150.w,
-                          child: Btn(
-                            ontap: () {
-                              ontap!();
-                              context.popPage();
-                            },
-                            text: isEdit ? "Edit" : "Add",
-                          ),
-                        ),
-                      ),
-                      spaceH(30),
-                    ],
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                spaceH(8),
+                Center(
+                  child: Container(
+                    width: 64.w,
+                    height: 4.h,
+                    color: AppColor.primary,
                   ),
                 ),
-              ),
-            ],
+                spaceH(20),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("title"),
+                        spaceH(8),
+                        CustomTextFiled(
+                          hint: "title",
+                          controller: titleController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "write your title";
+                            }
+                            return null;
+                          },
+                        ),
+                        spaceH(20),
+                        Text("subtitle"),
+                        spaceH(8),
+                        CustomTextFiled(
+                          hint: "subtitle",
+                          controller: subTitleController,
+                        ),
+                        spaceH(30),
+                        Center(
+                          child: SizedBox(
+                            width: 150.w,
+                            child: Btn(
+                              ontap: () {
+                                if (!formKey.currentState!.validate()) return;
+                                ontap!();
+                                context.popPage();
+                              },
+                              text: isEdit ? "Edit" : "Add",
+                            ),
+                          ),
+                        ),
+                        spaceH(30),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
